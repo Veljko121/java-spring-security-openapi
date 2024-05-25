0deb845fd1cd61e81b7bd6e0b12432f3f1c2ca92
Veljko Nikolić <nikolicveljko01@gmail.com>
2024-05-25 23:19:09 +0200

    Added schema.sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO _user(username, email, password, role)
 VALUES
-    ('veljko', 'nikolicveljko01@gmail.com', 'veljko', 0);
+    ('veljko', 'nikolicveljko01@gmail.com', '$2a$10$GvBT2HXPqMdOTKDbnjxEVOkvnABMUzNclJ3u91Qhcxysi8vxOK7/W', 0);
