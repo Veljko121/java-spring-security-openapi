9f8263df6d1857a05b2330fe36e1cb78b5b7070f
Veljko Nikolić <nikolicveljko01@gmail.com>
2024-11-25 17:36:20 +0100

    Passwords more clear

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+-- password = username
 INSERT INTO _user(username, email, password, role)
 VALUES
     ('veljko', 'nikolicveljko01@gmail.com', '$2a$10$GvBT2HXPqMdOTKDbnjxEVOkvnABMUzNclJ3u91Qhcxysi8vxOK7/W', 0);
