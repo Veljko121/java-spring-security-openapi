033c68d6f0205b510c8ec63f1ee69d986382e274
Veljko Nikolić <nikolicveljko01@gmail.com>
2024-11-25 17:43:33 +0100

    Added validation

--- src/main/java/com/veljko121/backend/dto/UserResponseDTO.java
+++ src/main/java/com/veljko121/backend/dto/UserResponseDTO.java
@@ -4,14 +4,26 @@ import java.time.LocalDateTime;
 
 import com.veljko121.backend.core.enums.Role;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Data;
 
 @Data
 public class UserResponseDTO {
     
+    @NotBlank
     private String username;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotEmpty
     private Role role;
+
+    @PastOrPresent
     private LocalDateTime createdDateTime;
 
 }
