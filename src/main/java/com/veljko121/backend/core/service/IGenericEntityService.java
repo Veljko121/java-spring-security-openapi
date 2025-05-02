3b65bad0136e9241f8b1c46d56410acfcc1cefe9
Veljko Nikolić <nikolicveljko01@gmail.com>
2025-05-02 22:13:53 +0200

    Simplified name

--- src/main/java/com/veljko121/backend/core/service/IGenericEntityService.java
+++ src/main/java/com/veljko121/backend/core/service/IGenericEntityService.java
@@ -2,6 +2,6 @@ package com.veljko121.backend.core.service;
 
 import com.veljko121.backend.core.model.GenericEntity;
 
-public interface IGenericEntityCRUDService<T extends GenericEntity> extends ICRUDService<T, Integer> {
+public interface IGenericEntityService<T extends GenericEntity> extends ICRUDService<T, Integer> {
     
 }
--- src/main/java/com/veljko121/backend/core/service/impl/GenericEntityService.java
+++ src/main/java/com/veljko121/backend/core/service/impl/GenericEntityService.java
@@ -3,9 +3,9 @@ package com.veljko121.backend.core.service.impl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.veljko121.backend.core.model.GenericEntity;
-import com.veljko121.backend.core.service.IGenericEntityCRUDService;
+import com.veljko121.backend.core.service.IGenericEntityService;
 
-public abstract class GenericEntityService<T extends GenericEntity> extends CRUDService<T, Integer> implements IGenericEntityCRUDService<T> {
+public abstract class GenericEntityService<T extends GenericEntity> extends CRUDService<T, Integer> implements IGenericEntityService<T> {
 
     public GenericEntityService(JpaRepository<T, Integer> repository) {
         super(repository);
